96b86eb59216943dc4dc4631be34ff382fef0b97
Lars Marius Garshol <larsga@gmail.com>
2011-06-06 18:34:21 +0200

    Added a Dice coefficient comparator.

---
+++ src/main/java/no/priv/garshol/duke/DiceCoefficientComparator.java
@@ -0,0 +1,44 @@
+
+package no.priv.garshol.duke;
+
+/**
+ * An implementation of the Dice coefficient using exact matching.
+ */
+public class DiceCoefficientComparator implements Comparator {
+  private Comparator subcomp;
+  
+  public DiceCoefficientComparator() {
+    this.subcomp = new ExactComparator();
+  }
+  
+  public boolean isTokenized() {
+    return true;
+  }
+
+  public double compare(String s1, String s2) {
+    if (s1.equals(s2))
+      return 1.0;
+
+    // tokenize
+    String[] t1 = StringUtils.split(s1);
+    String[] t2 = StringUtils.split(s2);
+
+    // ensure that t1 is shorter than or same length as t2
+    if (t1.length > t2.length) {
+      String[] tmp = t2;
+      t2 = t1;
+      t1 = tmp;
+    }
+    
+    // find best matches for each token in t1
+    double sum = 0;
+    for (int ix1 = 0; ix1 < t1.length; ix1++) {
+      double highest = 0;
+      for (int ix2 = 0; ix2 < t2.length; ix2++)
+        highest = Math.max(highest, subcomp.compare(t1[ix1], t2[ix2]));
+      sum += highest;
+    }
+
+    return (sum * 2) / (t1.length + t2.length);
+  }
+}
---
+++ src/test/java/no/priv/garshol/duke/test/DiceCoefficientComparatorTest.java
@@ -0,0 +1,38 @@
+
+package no.priv.garshol.duke.test;
+
+import org.junit.Test;
+import org.junit.Before;
+import static junit.framework.Assert.assertTrue;
+import static junit.framework.Assert.assertEquals;
+
+import no.priv.garshol.duke.DiceCoefficientComparator;
+
+public class DiceCoefficientComparatorTest {
+  private DiceCoefficientComparator comp;
+
+  @Before
+  public void setup() {
+    comp = new DiceCoefficientComparator();
+  }
+    
+  @Test
+  public void testEmpty() {
+    assertEquals(1.0, comp.compare("", ""));
+  }
+    
+  @Test
+  public void testOneIsEmpty() {
+    assertEquals(0.0, comp.compare("", "abc"));
+  }
+    
+  @Test
+  public void testOneIsDifferent() {
+    assertEquals(0.5, comp.compare("abc def", "cba def"));
+  }
+    
+  @Test
+  public void testReordering() {
+    assertEquals(1.0, comp.compare("def abc", "abc def"));
+  }
+}
