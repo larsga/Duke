41145481c95aef82add9e736af1633172df608cd
Lars Marius Garshol <larsga@gmail.com>
2011-06-12 03:54:53 +0200

    Added numeric comparator.

---
+++ src/main/java/no/priv/garshol/duke/NumericComparator.java
@@ -0,0 +1,42 @@
+
+package no.priv.garshol.duke;
+
+/**
+ * Comparator which compares two values numerically. The similarity is
+ * the ratio of the smaller number to the greater number.
+ */
+public class NumericComparator implements Comparator {
+  private double minratio;
+  
+  public boolean isTokenized() {
+    return false;
+  }
+
+  public void setMinRatio(double minratio) {
+    this.minratio = minratio;
+  }
+  
+  public double compare(String v1, String v2) {
+    double d1;
+    double d2;
+    try {
+      d1 = Double.parseDouble(v1);
+      d2 = Double.parseDouble(v2);
+    } catch (NumberFormatException e) {
+      return 0.0; // we just ignore this. whether it's wise I'm not sure
+    }
+
+    if (d2 < d1) {
+      double tmp = d2;
+      d2 = d1;
+      d1 = tmp;
+    }
+
+    double ratio = d1 / d2;
+    if (ratio < minratio)
+      return 0.0;
+    else
+      return ratio;
+  }
+  
+}
