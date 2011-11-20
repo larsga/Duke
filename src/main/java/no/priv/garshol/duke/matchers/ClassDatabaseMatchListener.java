1d714457e8e22e2ff3adf95eb7b1ac179f6fff54
Lars Marius Garshol <larsga@gmail.com>
2011-11-20 20:18:13 +0100

    Added listener to actually build eq class db.

---
+++ src/main/java/no/priv/garshol/duke/matchers/ClassDatabaseMatchListener.java
@@ -0,0 +1,40 @@
+
+package no.priv.garshol.duke.matchers;
+
+import no.priv.garshol.duke.Record;
+import no.priv.garshol.duke.Property;
+import no.priv.garshol.duke.Configuration;
+import no.priv.garshol.duke.EquivalenceClassDatabase;
+
+/**
+ * Writes recorded matches to an EquivalenceClassDatabase.
+ */
+public class ClassDatabaseMatchListener extends AbstractMatchListener {
+  private Configuration config;
+  private EquivalenceClassDatabase classdb;
+
+  public ClassDatabaseMatchListener(Configuration config,
+                                    EquivalenceClassDatabase classdb) {
+    this.config = config;
+    this.classdb = classdb;
+  }
+  
+  public void matches(Record r1, Record r2, double confidence) {
+    String id1 = getIdentity(r1);
+    String id2 = getIdentity(r2);
+    classdb.addLink(id1, id2);
+  }
+
+  public void batchDone() {
+    classdb.commit();
+  }
+  
+  private String getIdentity(Record r) {
+    for (Property p : config.getIdentityProperties())
+      for (String v : r.getValues(p.getName()))
+        return v;
+    throw new RuntimeException("No identity found in record [" +
+                               PrintMatchListener.toString(r) + "]");
+  }
+  
+}
