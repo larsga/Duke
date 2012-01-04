4c9c6d43d71f9d016be881d5595a6e179c258952
Lars Marius Garshol <larsga@gmail.com>
2012-01-04 11:48:09 +0100

    Removed duplicate test methods.

--- src/test/java/no/priv/garshol/duke/test/PersonNameCleanerTest.java
+++ src/test/java/no/priv/garshol/duke/test/PersonNameCleanerTest.java
@@ -15,63 +15,8 @@ public class PersonNameCleanerTest extends LowerCaseNormalizeCleanerTest {
   }
 
   @Test
-  public void testEmpty() {
-    assertEquals("", cleaner.clean(""));
-  }
-
-  @Test
-  public void testSingleChar() {
-    assertEquals("a", cleaner.clean("A"));
-  }
-
-  @Test
-  public void testSingleChar2() {
-    assertEquals("a", cleaner.clean("a"));
-  }
-
-  @Test
-  public void testSingleSpace() {
-    assertEquals("", cleaner.clean(" "));
-  }
-
-  @Test
-  public void testManySpaces() {
-    assertEquals("", cleaner.clean("    "));
-  }
-
-  @Test
-  public void testManyLeadingSpaces() {
-    assertEquals("a", cleaner.clean("    a"));
-  }
-
-  @Test
-  public void testManyTrailingSpaces() {
-    assertEquals("a", cleaner.clean("a    "));
-  }
-
-  @Test
-  public void testLarsMarius() {
-    assertEquals("lars marius", cleaner.clean("Lars Marius"));
-  }
-
-  @Test
-  public void testLarsMarius3Spaces() {
-    assertEquals("lars marius", cleaner.clean("Lars   Marius"));
-  }
-
-  @Test
-  public void testLarsMariusPadded() {
-    assertEquals("lars marius", cleaner.clean("   Lars   Marius   "));
-  }
-
-  @Test
   public void testMapping() {
     assertEquals("joseph stalin",
                  cleaner.clean("Joe Stalin"));
   }
-
-  @Test
-  public void testAccentStripping() {
-    assertEquals("male", cleaner.clean("Mal\u00E9"));
-  }  
 }
