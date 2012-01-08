f995ef73c66bfa73a20df3c001219ecb014cd5f8
Lars Marius Garshol <larsga@gmail.com>
2012-01-08 10:25:40 +0100

    Added test for a reported problem.

--- src/test/java/no/priv/garshol/duke/test/DatabaseTest.java
+++ src/test/java/no/priv/garshol/duke/test/DatabaseTest.java
@@ -3,6 +3,7 @@ package no.priv.garshol.duke.test;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.io.IOException;
 
 import org.junit.Test;
@@ -19,6 +20,7 @@ import no.priv.garshol.duke.comparators.ExactComparator;
 
 public class DatabaseTest {
   private Database db;
+  private Configuration config;
   
   @Before
   public void setup() throws IOException {
@@ -28,7 +30,7 @@ public class DatabaseTest {
     props.add(new Property("NAME", comp, 0.3, 0.8));
     props.add(new Property("EMAIL", comp, 0.3, 0.8));
 
-    Configuration config = new Configuration();
+    config = new Configuration();
     config.setProperties(props);
     config.setThreshold(0.85);
     config.setMaybeThreshold(0.8);
@@ -44,6 +46,18 @@ public class DatabaseTest {
     record = db.findRecordById("1");
     assertTrue("no record found", record != null);
     assertEquals("wrong ID", "1", record.getValue("ID"));
+  }
+
+  @Test
+  public void testBackslash() throws IOException {
+    String name = "'Lastname, Firstname \\(external\\)'";
+    Record record = TestUtils.makeRecord("ID", "1",
+                                         "NAME", name, "EMAIL", "BBBBB");
+    db.index(record);
+    db.commit();
+
+    Property prop = config.getPropertyByName("NAME");
+    db.lookup(prop, Collections.singleton("'lastname, firstname \\(external\\)'"));
   }
   
   @Test @Ignore
