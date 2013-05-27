5d3b3f2e547177d6f84ed1a4d8ffea1ca8292284
Lars Marius Garshol <larsga@gmail.com>
2013-05-27 13:30:42 +0200

    Implemented the --reindex option to DebugCompare.

--- src/main/java/no/priv/garshol/duke/DebugCompare.java
+++ src/main/java/no/priv/garshol/duke/DebugCompare.java
@@ -15,36 +15,52 @@ import java.io.BufferedReader;
 import org.xml.sax.SAXException;
 
 import no.priv.garshol.duke.utils.Utils;
+import no.priv.garshol.duke.utils.CommandLineParser;
 
 /**
  * Compare two specific records to understand their scores.
  */
 public class DebugCompare {
+  private static final int DEFAULT_BATCH_SIZE = 40000;
 
   public static void main(String[] argv) throws IOException, SAXException {
-    if (argv.length != 3) {
+    // parse command line
+    CommandLineParser parser = new CommandLineParser();
+    parser.setMinimumArguments(3);
+    parser.setMaximumArguments(3);
+    parser.registerOption(new CommandLineParser.BooleanOption("reindex", 'I'));
+    try {
+      argv = parser.parse(argv);
+    } catch (CommandLineParser.CommandLineParserException e) {
+      System.err.println("ERROR: " + e.getMessage());
       usage();
-      return;      
+      System.exit(1);
     }
+
+    // do we need to reindex?
+    boolean reindex = parser.getOptionState("reindex");
 
     // load configuration
     Configuration config = ConfigLoader.load(argv[0]);
-    Database database = config.createDatabase(false);
-    if (database.isInMemory()) {
-      System.err.println("ERROR: Database index is in-memory. Cannot look up " +
-                         "identities from earlier run");
-      return;
-    }
+    Database database = config.createDatabase(reindex); // overwrite iff reindex
+    if (database.isInMemory())
+      reindex = true; // no other way to do it in this case
+
+    // reindex, if requested
+    if (reindex)
+      reindex(config, database);
   
     // load records
     Record r1 = database.findRecordById(argv[1]);
     if (r1 == null) {
       System.err.println("Couldn't find record for '" + argv[1] + "'");
+      System.err.println("Consider using --reindex");
       return;
     }    
     Record r2 = database.findRecordById(argv[2]);
     if (r2 == null) {
       System.err.println("Couldn't find record for '" + argv[2] + "'");
+      System.err.println("Consider using --reindex");
       return;
     }
 
@@ -77,8 +93,8 @@ public class DebugCompare {
                                " (prob " + p + ")");
             high = Math.max(high, p);
           } catch (Exception e) {
-            throw new RuntimeException("Comparison of values '" + v1 + "' and "+
-                                       "'" + v2 + "' failed", e);
+            throw new DukeException("Comparison of values '" + v1 + "' and "+
+                                    "'" + v2 + "' failed", e);
           }
         }        
       }
@@ -95,6 +111,18 @@ public class DebugCompare {
     System.out.println("");
     System.out.println("java no.priv.garshol.duke.DebugCompare <cfgfile> <id1> <id2>");
     System.out.println("");
+    System.out.println("  --reindex: Reindex all records before comparing");
+  }
+
+  private static void reindex(Configuration config, Database database) {
+    System.out.println("Reindexing all records...");
+    Processor processor = new Processor(config, database);
+    if (config.isDeduplicationMode())
+      processor.index(config.getDataSources(), DEFAULT_BATCH_SIZE);
+    else {
+      processor.index(config.getDataSources(1), DEFAULT_BATCH_SIZE);
+      processor.index(config.getDataSources(2), DEFAULT_BATCH_SIZE);
+    }
   }
   
 }
