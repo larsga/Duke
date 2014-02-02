d82e58cc43569def13ed8614596d56616442a2b7
Lars Marius Garshol <larsga@gmail.com>
2014-02-02 14:57:09 +0100

    Fixed error in test file reader. Added test.

--- src/main/java/no/priv/garshol/duke/utils/LinkDatabaseUtils.java
+++ src/main/java/no/priv/garshol/duke/utils/LinkDatabaseUtils.java
@@ -1,6 +1,7 @@
 
 package no.priv.garshol.duke.utils;
 
+import java.io.Reader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.BufferedReader;
@@ -9,6 +10,7 @@ import no.priv.garshol.duke.Link;
 import no.priv.garshol.duke.LinkKind;
 import no.priv.garshol.duke.LinkStatus;
 import no.priv.garshol.duke.LinkDatabase;
+import no.priv.garshol.duke.DukeException;
 import no.priv.garshol.duke.InMemoryLinkDatabase;
 
 /**
@@ -27,12 +29,35 @@ public class LinkDatabaseUtils {
 
   /**
    * Loads a test file into an in-memory link database.
+   * @since 1.2
+   */
+  public static LinkDatabase loadTestFile(Reader reader) throws IOException {
+    LinkDatabase linkdb = new InMemoryLinkDatabase();
+    loadTestFile(reader, linkdb);
+    return linkdb;
+  }
+  
+  /**
+   * Loads a test file into an in-memory link database.
    */
   public static void loadTestFile(String testfile, LinkDatabase linkdb)
     throws IOException {
-    CSVReader reader = new CSVReader(new FileReader(testfile));
+    loadTestFile(new FileReader(testfile), linkdb);
+  }
+
+  /**
+   * Loads a test file into an in-memory link database.
+   * @since 1.2
+   */
+  public static void loadTestFile(Reader input, LinkDatabase linkdb)
+    throws IOException {
+    CSVReader reader = new CSVReader(input);
     String[] row = reader.next();
     while (row != null) {
+      if (row.length != 4)
+        throw new DukeException("Wrong test file format, row had " +
+                                row.length + " values, should be 4");
+        
       LinkKind kind = row[0].equals("+") ? LinkKind.SAME : LinkKind.DIFFERENT;
       String id1 = row[1];
       String id2 = row[2];
