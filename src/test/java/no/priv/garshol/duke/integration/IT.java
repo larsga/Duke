f39aaef3cac0b3e2eaeb8a11963f629414e67a05
Lars Marius Garshol <larsga@garshol.priv.no>
2014-02-16 13:35:31 +0100

    Added an integration test for the genetic algorithm.

--- src/test/java/no/priv/garshol/duke/integration/IT.java
+++ src/test/java/no/priv/garshol/duke/integration/IT.java
@@ -9,6 +9,7 @@ import java.io.InputStreamReader;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.rules.TemporaryFolder;
 import static org.junit.Assert.fail;
 import static org.junit.Assert.assertTrue;
@@ -27,7 +28,7 @@ public class IT {
 
   @Test
   public void testFailWithNoArguments() throws IOException {
-    Result r = run("");
+    Result r = duke("");
     
     assertEquals("didn't fail with error code", 1, r.code);
     assertTrue("Duke gave no error message: " + r.out,
@@ -36,7 +37,7 @@ public class IT {
 
   @Test
   public void testShowMatches() throws IOException {
-    Result r = run("--showmatches doc/example-data/countries.xml");
+    Result r = duke("--showmatches doc/example-data/countries.xml");
     
     assertEquals("failed with error code: " + r.out, 0, r.code);
     assertTrue("not enough matches", r.countOccurrences("MATCH 0.") > 50);
@@ -45,7 +46,7 @@ public class IT {
   @Test
   public void testMakeLinkFile() throws IOException {
     File linkfile = tmpdir.newFile();
-    Result r = run("--showmatches --linkfile=\"" + linkfile.getAbsolutePath() +
+    Result r = duke("--showmatches --linkfile=\"" + linkfile.getAbsolutePath() +
                    "\" doc/example-data/countries.xml");
 
     int outmatches = r.countOccurrences("MATCH 0.");
@@ -56,7 +57,7 @@ public class IT {
 
   @Test
   public void testShowData() throws IOException {
-    Result r = run("--showdata doc/example-data/countries.xml");
+    Result r = duke("--showdata doc/example-data/countries.xml");
 
     assertEquals("wrong number of IDs", 522, r.countOccurrences("ID: "));
     assertEquals("wrong number of NAMEs", 522, r.countOccurrences("NAME: "));
@@ -67,35 +68,53 @@ public class IT {
   // FIXME: weirdly, the test below always fails, even though it's doing
   //        exactly the same thing as the testMakeLinkFile test. no idea
   //        why.
-  // @Test
-  // public void testTestFile() throws IOException {
-  //   // run to make a link file first
-  //   File linkfile = tmpdir.newFile();
-  //   Result r = run("--linkfile=\"" + linkfile.getAbsolutePath() +
-  //                  "\" doc/example-data/countries.xml");
-  //   assertTrue("couldn't write link file: " + r.out, r.code == 0);
+  @Test @Ignore
+  public void testTestFile() throws IOException {
+    // run to make a link file first
+    File linkfile = tmpdir.newFile();
+    Result r = duke("--linkfile=\"" + linkfile.getAbsolutePath() +
+                   "\" doc/example-data/countries.xml");
+    assertTrue("couldn't write link file: " + r.out, r.code == 0);
 
-  //   // now we match against the test file
-  //   r = run("--testfile=\"" + linkfile.getAbsolutePath() +
-  //           "\" doc/example-data/countries.xml");
+    // now we match against the test file
+    r = duke("--testfile=\"" + linkfile.getAbsolutePath() +
+            "\" doc/example-data/countries.xml");
 
-  //   assertEquals("failed with error code: " + r.out, 0, r.code);
-  //   assertTrue("Can't find precision output: " + r.out,
-  //              r.out.indexOf("Precision ") != -1);
-  // }
+    assertEquals("failed with error code: " + r.out, 0, r.code);
+    assertTrue("Can't find precision output: " + r.out,
+               r.out.indexOf("Precision ") != -1);
+  }
+
+  @Test
+  public void testGenetic() throws IOException {
+    Result r = genetic("--testfile=doc/example-data/countries-test.txt doc/example-data/countries.xml");
+    assertEquals("failed with error code: " + r.out, 0, r.code);
+    assertEquals("Didn't run for 100 generations", 100,
+                 r.countOccurrences("BEST: "));
+    assertTrue("couldn't find a good solution",
+               r.floatAfterLast("BEST: ") > 0.95);
+  }
   
   // ===== UTILITIES
 
-  private Result run(String args) throws IOException {
+  private Result duke(String args) throws IOException {
+    return run("Duke", args);
+  }
+
+  private Result genetic(String args) throws IOException {
+    return run("genetic.Driver", args);
+  }
+  
+  private Result run(String klass, String args) throws IOException {
     String jar = "target/duke-" + Duke.getVersion() + ".jar";
     Process p = Runtime.getRuntime().exec("java -cp " + jar +
-                                          " no.priv.garshol.duke.Duke " +
-                                          args);
+                                          " no.priv.garshol.duke." + klass +
+                                          " " + args);
     StringBuilder tmp = new StringBuilder();
     BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
     String line;
     while ((line = r.readLine()) != null)
-      tmp.append(line);
+      tmp.append(line + " ");
     r.close();
 
     r = new BufferedReader(new InputStreamReader(p.getErrorStream()));
@@ -130,6 +149,28 @@ public class IT {
         count++;
         pos += sub.length();
       }
+    }
+
+    public float floatAfterLast(String sub) {
+      // first, scan to find last occurrence of 'sub'
+      int pos = 0;
+      while (true) {
+        int postpos = out.indexOf(sub, pos);
+        if (postpos == -1)
+          break;
+        pos = postpos + sub.length();
+      }
+
+      // then parse the float
+      int ix;
+      for (ix = pos; ix < out.length(); ix++) {
+        char ch = out.charAt(ix);
+        if (ch != '.' && (ch < '0' || ch > '9'))
+          break;
+      }
+
+      // finally
+      return Float.valueOf(out.substring(pos, ix));
     }
   }
 }
