3ec29446259c383fc3cd8915d179756ac52b8c83
fabriziofortino <fabrizio.fortino@gmail.com>
2015-03-19 00:53:21 +0000

    RecordImpl: added equals and hashCode

--- duke-core/src/main/java/no/priv/garshol/duke/RecordImpl.java
+++ duke-core/src/main/java/no/priv/garshol/duke/RecordImpl.java
@@ -1,9 +1,7 @@
-
 package no.priv.garshol.duke;
 
 import java.util.Map;
 import java.util.HashMap;
-import java.util.List;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -65,5 +63,30 @@ public class RecordImpl implements ModifiableRecord {
 
   public String toString() {
     return "[RecordImpl " + data + "]";
+  }
+
+  @Override
+  public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + ((data == null) ? 0 : data.hashCode());
+	return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+	if (this == obj)
+	  return true;
+	if (obj == null)
+	  return false;
+	if (getClass() != obj.getClass())
+      return false;
+	RecordImpl other = (RecordImpl) obj;
+	if (data == null) {
+	  if (other.data != null)
+	    return false;
+	} else if (!data.equals(other.data))
+	  return false;
+	return true;
   }
 }
