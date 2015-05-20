7b314b8d6d7796a0e5756741ab516bd7beb2d1cf
Lars Marius Garshol <larsga@garshol.priv.no>
2015-05-20 15:24:22 +0200

    Made new pull request conform to style

--- duke-core/src/main/java/no/priv/garshol/duke/datasources/SparqlDataSource.java
+++ duke-core/src/main/java/no/priv/garshol/duke/datasources/SparqlDataSource.java
@@ -13,232 +13,230 @@ import no.priv.garshol.duke.utils.SparqlClient;
 import no.priv.garshol.duke.utils.SparqlResult;
 
 public class SparqlDataSource extends ColumnarDataSource {
-    private static final int DEFAULT_PAGE_SIZE = 1000;
-    private String endpoint;
-    private String query;
+  private static final int DEFAULT_PAGE_SIZE = 1000;
+  private String endpoint;
+  private String query;
+  private String username; // for HTTP authentication
+  private String password; // for HTTP authentication
+  protected int pagesize; // protected for test purposes
+  /**
+   * In triple mode we expect query results to be of the form:
+   * (subject, property, value), whereas in normal mode we treat the
+   * query as tabular (ie: one row per subject).
+   */
+  private boolean triple_mode;
 
+  public SparqlDataSource() {
+    this.pagesize = DEFAULT_PAGE_SIZE;
+    this.triple_mode = true;
+  }
 
-    private String username;
-    private String password;
-    protected int pagesize; // protected for test purposes
-    /**
-     * In triple mode we expect query results to be of the form:
-     * (subject, property, value), whereas in normal mode we treat the
-     * query as tabular (ie: one row per subject).
-     */
-    private boolean triple_mode;
+  public void setEndpoint(String endpoint) {
+    this.endpoint = endpoint;
+  }
 
-    public SparqlDataSource() {
-        this.pagesize = DEFAULT_PAGE_SIZE;
-        this.triple_mode = true;
+  public void setQuery(String query) {
+    this.query = query;
+  }
+
+  /**
+   * If pagesize is set to 0, paging is disabled.
+   */
+  public void setPageSize(int pagesize) {
+    this.pagesize = pagesize;
+  }
+
+  public void setTripleMode(boolean triple_mode) {
+    this.triple_mode = triple_mode;
+  }
+
+  public int getPageSize() {
+    return pagesize;
+  }
+
+  public boolean getTripleMode() {
+    return triple_mode;
+  }
+
+  public String getQuery() {
+    return query;
+  }
+
+  public String getEndpoint() {
+    return endpoint;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public RecordIterator getRecords() {
+    verifyProperty(endpoint, "endpoint");
+    verifyProperty(query, "query");
+
+    if (triple_mode)
+      return new TripleModeIterator();
+    else
+      return new TabularIterator();
+  }
+
+  @Override
+  public void writeConfig(ConfigWriter cw) {
+    final String name = "sparql";
+    cw.writeStartElement(name, null);
+
+    cw.writeParam("endpoint", getEndpoint());
+    cw.writeParam("query", getQuery());
+    cw.writeParam("page-size", getPageSize());
+    cw.writeParam("triple-mode", getTripleMode());
+
+    //Input required for Basic Authorization
+    cw.writeParam("username", getUsername());
+    cw.writeParam("password", getPassword());
+
+    writeColumnsConfig(cw);
+
+    cw.writeEndElement(name);
+  }
+
+  protected String getSourceName() {
+    return "SPARQL";
+  }
+
+  /**
+   * An extension point so we can control how the query gets executed.
+   * This exists for testing purposes, not because we believe it will
+   * actually be used for real.
+   */
+  public SparqlResult runQuery(String endpoint, String query) {
+    return SparqlClient.execute(endpoint, query, username, password);
+  }
+
+  // --- SparqlIterator
+
+  abstract class SparqlIterator extends RecordIterator {
+    protected int pageno;
+    protected int pagerow;
+    protected List<String> variables;
+    protected List<String[]> page;
+    protected RecordBuilder builder;
+
+    public SparqlIterator() {
+      this.builder = new RecordBuilder(SparqlDataSource.this);
+      fetchNextPage();
     }
 
-    public void setEndpoint(String endpoint) {
-        this.endpoint = endpoint;
+    public boolean hasNext() {
+      return pagerow < page.size();
     }
 
-    public void setQuery(String query) {
-        this.query = query;
+    public void remove() {
+      throw new UnsupportedOperationException();
     }
 
-    /**
-     * If pagesize is set to 0, paging is disabled.
-     */
-    public void setPageSize(int pagesize) {
-        this.pagesize = pagesize;
+    protected void fetchNextPage() {
+      if (pagesize == 0 && pageno > 0) {
+        // paging is turned off, and we've been asked to get the second page
+        page = Collections.EMPTY_LIST;
+        pagerow = 0;
+        pageno++;
+        return;
+      }
+
+      String thisquery = query;
+      if (pagesize != 0) // paging is turned off
+        thisquery += (" limit " + pagesize + " offset " + (pageno * pagesize));
+
+      if (logger != null)
+        logger.debug("SPARQL query: " + thisquery);
+
+      SparqlResult result = runQuery(endpoint, thisquery);
+      variables = result.getVariables();
+      page = result.getRows();
+
+      if (triple_mode && !page.isEmpty() && page.get(0).length != 3)
+        throw new DukeConfigException("In triple mode SPARQL queries must " +
+                                      "produce exactly three columns!");
+
+      if (logger != null)
+        logger.debug("SPARQL result rows: " + page.size());
+
+      pagerow = 0;
+      pageno++;
     }
 
-    public void setTripleMode(boolean triple_mode) {
-        this.triple_mode = triple_mode;
+    protected void addValue(int valueix, Column col) {
+      if (col == null)
+        return;
+
+      String value = page.get(pagerow)[valueix];
+      builder.addValue(col, value);
     }
+  }
 
-    public int getPageSize() {
-        return pagesize;
-    }
+  class TripleModeIterator extends SparqlIterator {
 
-    public boolean getTripleMode() {
-        return triple_mode;
-    }
+    public Record next() {
+      String resource = page.get(pagerow)[0];
 
-    public String getQuery() {
-        return query;
-    }
+      Collection<Column> cols = columns.get("?uri");
+      if (cols == null)
+        throw new DukeConfigException("No '?uri' column. It's required in triple mode");
+      Column uricol = cols.iterator().next();
 
-    public String getEndpoint() {
-        return endpoint;
-    }
+      builder.newRecord();
+      builder.setValue(uricol, resource);
 
-    public String getUsername() {
-        return username;
-    }
+      while (pagerow < page.size() && resource.equals(page.get(pagerow)[0])) {
+        while (pagerow < page.size() && resource.equals(page.get(pagerow)[0])) {
+          cols = columns.get(page.get(pagerow)[1]);
+          if (cols != null) {
+            for (Column col : cols)
+              addValue(2, col);
+          }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public RecordIterator getRecords() {
-        verifyProperty(endpoint, "endpoint");
-        verifyProperty(query, "query");
-
-        if (triple_mode)
-            return new TripleModeIterator();
-        else
-            return new TabularIterator();
-    }
-
-    @Override
-    public void writeConfig(ConfigWriter cw) {
-        final String name = "sparql";
-        cw.writeStartElement(name, null);
-
-        cw.writeParam("endpoint", getEndpoint());
-        cw.writeParam("query", getQuery());
-        cw.writeParam("page-size", getPageSize());
-        cw.writeParam("triple-mode", getTripleMode());
-
-        //Input required for Basic Authorization
-        cw.writeParam("username", getUsername());
-        cw.writeParam("password", getPassword());
-
-        writeColumnsConfig(cw);
-
-        cw.writeEndElement(name);
-    }
-
-    protected String getSourceName() {
-        return "SPARQL";
-    }
-
-    /**
-     * An extension point so we can control how the query gets executed.
-     * This exists for testing purposes, not because we believe it will
-     * actually be used for real.
-     */
-    public SparqlResult runQuery(String endpoint, String query) {
-        return SparqlClient.execute(endpoint, query, username, password);
-    }
-
-    // --- SparqlIterator
-
-    abstract class SparqlIterator extends RecordIterator {
-        protected int pageno;
-        protected int pagerow;
-        protected List<String> variables;
-        protected List<String[]> page;
-        protected RecordBuilder builder;
-
-        public SparqlIterator() {
-            this.builder = new RecordBuilder(SparqlDataSource.this);
-            fetchNextPage();
+          pagerow++;
         }
 
-        public boolean hasNext() {
-            return pagerow < page.size();
-        }
+        // did we just step off this page?
+        if (pagerow >= page.size())
+          fetchNextPage();
+      }
 
-        public void remove() {
-            throw new UnsupportedOperationException();
-        }
-
-        protected void fetchNextPage() {
-            if (pagesize == 0 && pageno > 0) {
-                // paging is turned off, and we've been asked to get the second page
-                page = Collections.EMPTY_LIST;
-                pagerow = 0;
-                pageno++;
-                return;
-            }
-
-            String thisquery = query;
-            if (pagesize != 0) // paging is turned off
-                thisquery += (" limit " + pagesize + " offset " + (pageno * pagesize));
-
-            if (logger != null)
-                logger.debug("SPARQL query: " + thisquery);
-
-            SparqlResult result = runQuery(endpoint, thisquery);
-            variables = result.getVariables();
-            page = result.getRows();
-
-            if (triple_mode && !page.isEmpty() && page.get(0).length != 3)
-                throw new DukeConfigException("In triple mode SPARQL queries must " +
-                        "produce exactly three columns!");
-
-            if (logger != null)
-                logger.debug("SPARQL result rows: " + page.size());
-
-            pagerow = 0;
-            pageno++;
-        }
-
-        protected void addValue(int valueix, Column col) {
-            if (col == null)
-                return;
-
-            String value = page.get(pagerow)[valueix];
-            builder.addValue(col, value);
-        }
+      return builder.getRecord();
     }
+  }
 
-    class TripleModeIterator extends SparqlIterator {
+  class TabularIterator extends SparqlIterator {
 
-        public Record next() {
-            String resource = page.get(pagerow)[0];
+    public Record next() {
+      builder.newRecord();
 
-            Collection<Column> cols = columns.get("?uri");
-            if (cols == null)
-                throw new DukeConfigException("No '?uri' column. It's required in triple mode");
-            Column uricol = cols.iterator().next();
+      for (int colix = 0; colix < variables.size(); colix++) {
+        Collection<Column> cols = columns.get(variables.get(colix));
+        if (cols != null)
+          for (Column col : cols)
+            addValue(colix, col);
+      }
 
-            builder.newRecord();
-            builder.setValue(uricol, resource);
+      pagerow++;
+      // do we need to load the next page?
+      if (pagerow >= page.size())
+        fetchNextPage();
 
-            while (pagerow < page.size() && resource.equals(page.get(pagerow)[0])) {
-                while (pagerow < page.size() && resource.equals(page.get(pagerow)[0])) {
-                    cols = columns.get(page.get(pagerow)[1]);
-                    if (cols != null) {
-                        for (Column col : cols)
-                            addValue(2, col);
-                    }
-
-                    pagerow++;
-                }
-
-                // did we just step off this page?
-                if (pagerow >= page.size())
-                    fetchNextPage();
-            }
-
-            return builder.getRecord();
-        }
+      return builder.getRecord();
     }
-
-    class TabularIterator extends SparqlIterator {
-
-        public Record next() {
-            builder.newRecord();
-
-            for (int colix = 0; colix < variables.size(); colix++) {
-                Collection<Column> cols = columns.get(variables.get(colix));
-                if (cols != null)
-                    for (Column col : cols)
-                        addValue(colix, col);
-            }
-
-            pagerow++;
-            // do we need to load the next page?
-            if (pagerow >= page.size())
-                fetchNextPage();
-
-            return builder.getRecord();
-        }
-    }
+  }
 }
--- duke-core/src/main/java/no/priv/garshol/duke/utils/SparqlClient.java
+++ duke-core/src/main/java/no/priv/garshol/duke/utils/SparqlClient.java
@@ -23,127 +23,124 @@ import org.xml.sax.helpers.XMLReaderFactory;
 import org.xml.sax.helpers.DefaultHandler;
 
 public class SparqlClient {
-    private static final String SPARQL_NS =
-            "http://www.w3.org/2005/sparql-results#";
+  private static final String SPARQL_NS =
+    "http://www.w3.org/2005/sparql-results#";
 
-    public static SparqlResult execute(String endpoint, String query, String username, String password) {
-        query = urlencode(query);
-        String url = endpoint + "?query=" + query;
-        return loadResultSet(getResponse(url, username, password));
+  public static SparqlResult execute(String endpoint, String query, String username, String password) {
+    query = urlencode(query);
+    String url = endpoint + "?query=" + query;
+    return loadResultSet(getResponse(url, username, password));
+  }
+
+  public static SparqlResult loadResultSet(InputSource source) {
+    try {
+      ResultHandler handler = new ResultHandler();
+      XMLReader parser = XMLReaderFactory.createXMLReader();
+      parser.setContentHandler(handler);
+      parser.parse(source);
+      return handler.getResults();
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    } catch (SAXException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private static InputSource getResponse(String url, String username, String password) {
+    try {
+      URL urlobj = new URL(url);
+      HttpURLConnection conn = (HttpURLConnection) urlobj.openConnection();
+
+      // Basic Authentication
+      if (username != null && password != null) {
+        byte[] buf = (username + ":" + password).getBytes("utf-8");
+        String encoding = new sun.misc.BASE64Encoder().encode(buf);
+        conn.setRequestProperty("Authorization", "Basic " + encoding);
+      }
+
+      conn.setRequestProperty("Accept", "application/sparql-results+xml");
+      return new InputSource(conn.getInputStream());
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private static String urlencode(String url) {
+    try {
+      return URLEncoder.encode(url, "utf-8");
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private static class ResultHandler extends DefaultHandler {
+    private SparqlResult result;
+    private String[] currentRow;
+
+    private Map<String, Integer> columnIndexes;
+    private int colix;
+    private String curvar;
+
+    private Set<String> keepers;
+    private boolean keep;
+    private StringBuffer content;
+
+    private ResultHandler() {
+      this.result = new SparqlResult();
+
+      this.columnIndexes = new HashMap();
+      this.colix = 0;
+
+      this.keepers = new HashSet();
+      this.content = new StringBuffer();
+      keepers.add("uri");
+      keepers.add("literal");
+      keepers.add("bnode");
     }
 
-    public static SparqlResult loadResultSet(InputSource source) {
-        try {
-            ResultHandler handler = new ResultHandler();
-            XMLReader parser = XMLReaderFactory.createXMLReader();
-            parser.setContentHandler(handler);
-            parser.parse(source);
-            return handler.getResults();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (SAXException e) {
-            throw new RuntimeException(e);
-        }
+    public SparqlResult getResults() {
+      return result;
     }
 
-    private static InputSource getResponse(String url, String username, String password) {
-        try {
-            URL urlobj = new URL(url);
-            HttpURLConnection conn = (HttpURLConnection) urlobj.openConnection();
+    public void startElement(String uri, String localName, String qName,
+                             Attributes attributes) {
+      if (!SPARQL_NS.equals(uri))
+        return; // not SPARQL namespace, so we don't know what it is
 
+      if (localName.equals("variable")) {
+        String var = attributes.getValue("name");
+        columnIndexes.put(var, colix++);
+        result.addVariable(var);
 
-            // Basic Authorization
-            if (username != null && password != null) {
+      } else if (localName.equals("result"))
+        currentRow = new String[columnIndexes.size()];
 
-                String userPassword = username + ":" + password;
-                String encoding = new sun.misc.BASE64Encoder().encode(userPassword.getBytes());
-                conn.setRequestProperty("Authorization", "Basic " + encoding);
+      else if (localName.equals("binding"))
+        curvar = attributes.getValue("name");
 
-            }
-
-            conn.setRequestProperty("Accept", "application/sparql-results+xml");
-            return new InputSource(conn.getInputStream());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+      else if (keepers.contains(localName)) {
+        keep = true;
+        content.setLength(0);
+      }
     }
 
-    private static String urlencode(String url) {
-        try {
-            return URLEncoder.encode(url, "utf-8");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+    public void characters(char[] ch, int start, int length) {
+      if (keep)
+        content.append(ch, start, length);
     }
 
-    private static class ResultHandler extends DefaultHandler {
-        private SparqlResult result;
-        private String[] currentRow;
+    public void endElement(String uri, String localName, String qName) {
+      if (!SPARQL_NS.equals(uri))
+        return; // not SPARQL namespace, so we don't know what it is
 
-        private Map<String, Integer> columnIndexes;
-        private int colix;
-        private String curvar;
-
-        private Set<String> keepers;
-        private boolean keep;
-        private StringBuffer content;
-
-        private ResultHandler() {
-            this.result = new SparqlResult();
-
-            this.columnIndexes = new HashMap();
-            this.colix = 0;
-
-            this.keepers = new HashSet();
-            this.content = new StringBuffer();
-            keepers.add("uri");
-            keepers.add("literal");
-            keepers.add("bnode");
-        }
-
-        public SparqlResult getResults() {
-            return result;
-        }
-
-        public void startElement(String uri, String localName, String qName,
-                                 Attributes attributes) {
-            if (!SPARQL_NS.equals(uri))
-                return; // not SPARQL namespace, so we don't know what it is
-
-            if (localName.equals("variable")) {
-                String var = attributes.getValue("name");
-                columnIndexes.put(var, colix++);
-                result.addVariable(var);
-
-            } else if (localName.equals("result"))
-                currentRow = new String[columnIndexes.size()];
-
-            else if (localName.equals("binding"))
-                curvar = attributes.getValue("name");
-
-            else if (keepers.contains(localName)) {
-                keep = true;
-                content.setLength(0);
-            }
-        }
-
-        public void characters(char[] ch, int start, int length) {
-            if (keep)
-                content.append(ch, start, length);
-        }
-
-        public void endElement(String uri, String localName, String qName) {
-            if (!SPARQL_NS.equals(uri))
-                return; // not SPARQL namespace, so we don't know what it is
-
-            if (localName.equals("binding")) {
-                int ix = columnIndexes.get(curvar);
-                currentRow[ix] = content.toString();
-            } else if (localName.equals("result"))
-                result.addRow(currentRow);
-            else if (keepers.contains(localName))
-                keep = false;
-        }
+      if (localName.equals("binding")) {
+        int ix = columnIndexes.get(curvar);
+        currentRow[ix] = content.toString();
+      } else if (localName.equals("result"))
+        result.addRow(currentRow);
+      else if (keepers.contains(localName))
+        keep = false;
     }
+  }
 
 }
